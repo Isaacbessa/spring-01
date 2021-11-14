194ed2284e29e1b93d9fc991c5e357468704e2f2
Isaacbessa <Isaacbessa01@gmail.com>
2021-11-14 19:51:14 -0300

    finalizando

--- src/main/java/com/api/produto/controle/ProdutoControle.java
+++ src/main/java/com/api/produto/controle/ProdutoControle.java
@@ -3,6 +3,8 @@ package com.api.produto.controle;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,19 +12,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.produto.modelo.ProdutoModelo;
+import com.api.produto.modelo.RespostaModelo;
 import com.api.produto.repositorio.ProdutoRepositorio;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost")
 public class ProdutoControle {
 	//@ResponseBody: indicando que um parâmetro de método deve ser associado ao corpo da solicitação HTTP.
 	//métodos sendo feitos
 		@Autowired
 		private ProdutoRepositorio acoes;
-	
-	
 
-//Listrar produtos
+//Listar produtos
 
 	//Inicio
 @RequestMapping(value="/produtos", method=RequestMethod.GET)// get = select
@@ -35,5 +37,32 @@ public class ProdutoControle {
 @RequestMapping(value ="/produtos", method=RequestMethod.POST)
 	public @ResponseBody ProdutoModelo cadastrar(@RequestBody ProdutoModelo produto) {
 					return acoes.save(produto);	
+	}
+
+//Filtrar produtos
+@RequestMapping(value="/produtos{codigo}", method = RequestMethod.GET)
+public @ResponseBody ProdutoModelo filtrar(@PathVariable Integer codigo){
+	return acoes.findByCodigo(codigo);
+ }
+
+//Alterar produtos
+@RequestMapping(value="/produtos", method = RequestMethod.PUT)
+public @ResponseBody ProdutoModelo alterar(@RequestBody ProdutoModelo produto) {
+		return acoes.save(produto);
+ }
+
+//Remover produtos
+@RequestMapping(value="/produtos/{codigo}", method = RequestMethod.DELETE)
+	public @ResponseBody RespostaModelo remover(@PathVariable Integer codigo) {
+	//criando uma instancia
+		RespostaModelo resposta = new RespostaModelo();
+		try {
+		ProdutoModelo produto = filtrar(codigo);
+		this.acoes.delete(produto);
+		resposta.setMensagem("Produto removido com sucesso");
+		} catch(Exception erro) {
+			resposta.setMensagem("Falha ao remover" + erro.getMessage());
+		}
+		return resposta;
 	}
 }
---
+++ src/main/java/com/api/produto/modelo/RespostaModelo.java
@@ -0,0 +1,16 @@
+package com.api.produto.modelo;
+
+public class RespostaModelo {
+		//atribuito
+	private String mensagem;
+
+	// get e set
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	
+}
